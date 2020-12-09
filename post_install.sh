5a33268cc1cf3cfa12a244f791df93f6cc3a53a5
Rongsong Shen <shenrs@cn.ibm.com>
2020-12-09 14:45:36 +0800

    Generic

--- post_install.sh
+++ post_install.sh
@@ -1,8 +1,25 @@
 #!/bin/sh
 
-generic=/root/generic.sh
-
-if [ -f $generic ]; then
-   chmod a+x $generic
-   $generic
+_apply=/root/bin/apply_role.sh
+if [ ! -f $_apply ]; then
+    echo Can not find $_apply
+    exit 1
 fi
+
+case `hostname` in
+    login*)
+        _role=jails/login 
+        ;;
+    workstation*)
+        _role=jails/workstation
+        ;;
+    *)
+        ;;
+esac
+
+if [ "X$_role" = "X" ]; then
+    echo unknown role for `hostname`
+    exit 1
+fi
+
+sh $_apply $_role setup
